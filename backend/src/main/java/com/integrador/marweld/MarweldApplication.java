a9625d434b6d441aed65cc494a8a606a298c0f1e
Omar-Cc <U22227800@utp.edu.pe>
2026-05-31 21:28:28 -0500

    iniciando proyecto

---
+++ backend/src/main/java/com/integrador/marweld/MarweldApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.marweld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarweldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarweldApplication.class, args);
+	}
+
+}
